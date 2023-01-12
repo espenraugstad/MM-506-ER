9494dc7c5e0911c7dd88ca2e4a9ce98ccbcc8449
espenraugstad <espen.raugstad@gmail.com>
2023-01-12 18:20:10 +0100

    Redesigned dashboard

--- public/scripts/presenter-dashboard.js
+++ public/scripts/presenter-dashboard.js
@@ -7,12 +7,16 @@ const logoutBtn = document.getElementById("logout-btn");
 const createPresentationButton = document.getElementById(
   "create-presentation-button"
 );
+const editPresentation = document.getElementById("edit-presentation");
+const present = document.getElementById("present");
 
 /***** GLOBAL VARIABLES *****/
 let username = localStorage.getItem("user");
 let userId = localStorage.getItem("userId");
+let selectedPresentationId = -1;
 
 window.onload = async () => {
+  1;
   setUsername();
   await displayPresentations();
 };
@@ -25,7 +29,79 @@ async function displayPresentations() {
   let presentations = await getPresentations();
   presentationList.innerHTML = "";
 
-  for (let presentation of presentations) {
+  for (let p of presentations) {
+    let deckLine = document.createElement("div");
+    deckLine.classList.add("deckline");
+
+    let deckTitle = document.createElement("div");
+    deckTitle.classList.add("deckline__title");
+    deckTitle.innerHTML = p.presentation_title;
+    deckLine.appendChild(deckTitle);
+
+    let deleteButton = document.createElement("div");
+    deleteButton.classList.add("delete-presentation");
+    deleteButton.innerHTML =
+      '<span class="material-symbols-outlined">delete</span>';
+    deckLine.appendChild(deleteButton);
+
+    presentationList.appendChild(deckLine);
+
+    deckTitle.addEventListener("click", () => {
+      console.log(p.presentation_id);
+      console.log(selectedPresentationId);
+
+      if (selectedPresentationId !== p.presentation_id) {
+        console.log("Another");
+        // Turn of any other selections
+        let selected = document.querySelector(".deckline--selected");
+        console.log(selected);
+        if (selected) {
+          selected.classList.remove("deckline--selected");
+        }
+        deckLine.classList.add("deckline--selected");
+      } else {
+        console.log("Same");
+        deckLine.classList.toggle("deckline--selected");
+      }
+      selectedPresentationId = p.presentation_id;
+
+      console.log(deckLine);
+      // Make nav buttons active or inactive
+      if (deckLine.classList.contains("deckline--selected")) {
+        let selectedBtns = document.querySelectorAll(".dashboard--action");
+        for (let el of selectedBtns) {
+          el.classList.remove("btn--inactive");
+        }
+      } else {
+        let selectedBtns = document.querySelectorAll(".dashboard--action");
+        for (let el of selectedBtns) {
+          el.classList.add("btn--inactive");
+        }
+      }
+    });
+
+    deleteButton.addEventListener("click", async () => {
+      console.log(`Deleting ${p.presentation_title}`);
+      let action = await modalMessage(
+        "Delete",
+        `Are you sure you want to delete the presentation "${p.presentation_title}"?`,
+        [
+          { text: "Cancel", returnValue: false },
+          { text: "Delete", returnValue: true },
+        ]
+      );
+      destroyModal();
+      if (action) {
+        console.log("Deleting");
+        await deletePresentation(p.presentation_id);
+        await displayPresentations();
+      } else {
+        return;
+      }
+    });
+  }
+
+  /*   for (let presentation of presentations) {
     //console.log(presentation);
     let card = document.createElement("div");
     card.classList.add("presentation-card");
@@ -77,22 +153,22 @@ async function displayPresentations() {
     });
 
     presentationList.appendChild(card);
-  }
+  } */
 }
 
-async function deletePresentation(presentationId){
+async function deletePresentation(presentationId) {
   console.log(`Deleting presentation ${presentationId}`);
   const url = "/deletePresentation";
   const cfg = {
-    method: "post", 
+    method: "post",
     headers: {
-      "content-type": "application/json"
+      "content-type": "application/json",
     },
-    body: JSON.stringify({"presentationId": presentationId})
-  }
+    body: JSON.stringify({ presentationId: presentationId }),
+  };
   let result = await fetch(url, cfg);
   console.log(result);
-  let data = await result.json()
+  let data = await result.json();
   console.log(data);
 }
 
@@ -139,4 +215,17 @@ createPresentationButton.addEventListener("click", async () => {
     console.log(result.status);
   }
   console.log(data);
+});
+
+editPresentation.addEventListener("click", () => {
+  if (document.querySelector(".deckline--selected")) {
+    location.href = `authoring.html?presentation=${selectedPresentationId}`;
+  }
+});
+
+present.addEventListener("click", () => {
+  if (document.querySelector(".deckline--selected")) {
+    localStorage.setItem("presentationId", selectedPresentationId);
+    location.href = "presenter.html";
+  }
 });
