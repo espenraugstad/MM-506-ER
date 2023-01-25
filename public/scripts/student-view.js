119a8bb3bd6d339222c35abd30ce8bc2eb82b896
espenraugstad <espen.raugstad@gmail.com>
2023-01-25 11:43:25 +0100

    Student view ok without interactivity

--- public/scripts/student-view.js
+++ public/scripts/student-view.js
@@ -1,5 +1,6 @@
 /***** HTML ELEMENTS *****/
 const presentationTitle = document.getElementById("presentation-title");
+const previewBar = document.getElementById("previews");
 
 /***** GLOBAL VARIABLES *****/
 let presentationId = -1;
@@ -17,7 +18,46 @@ window.onload = async () => {
   currentPresentation = await getPresentation();
   setTitle();
   console.log(currentPresentation);
+
+  updatePreview();
 };
+
+function updatePreview() {  
+  console.log("Updating pres");
+  previewBar.innerHTML = "";
+  if (currentPresentation) {
+    
+    //parsedPresentation = marked.parse(currentPresentation.markdown);
+    let slides = marked.parse(currentPresentation.markdown).split("<hr>");
+
+    for (let slide of slides) {
+      // Div to contain the slide preview
+      let slideDiv = document.createElement("div");
+      slideDiv.classList.add("preview-slide");
+
+      // Parse the html of the slide to an HTML document
+      const domParser = new DOMParser();
+      let slideHtml = domParser.parseFromString(slide, "text/html");
+      // The first element will be in the preview
+      let previewText = "";
+      if (slideHtml.body.firstChild) {
+        let previewText = slideHtml.body.firstChild.textContent;
+        // Add the text to the slide (in a separate div?)
+        if (previewText !== "") {
+          slideDiv.innerHTML = `<div class="title-medium">${previewText}<div>`;
+        } else {
+          slideDiv.innerHTML = `<div class="title-medium">&lt;This slide is empty&gt;<div>`;
+        }
+      } else {
+        slideDiv.innerHTML = `<div class="title-medium">&lt;This slide is empty&gt;<div>`;
+      }
+
+      previewBar.appendChild(slideDiv);
+    }
+  } else {
+    console.log("No presentation exists.");
+  }
+}
 
 async function getPresentation() {
   let serverData = await fetch(`/getPresentation/${presentationId}`, {
