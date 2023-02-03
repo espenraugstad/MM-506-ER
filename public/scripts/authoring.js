e51c22dabcab5f08ba672afaaf99fe51fc1f2b4c
espenraugstad <espen.raugstad@gmail.com>
2023-02-03 15:54:07 +0100

    Started adding image placement

--- public/scripts/authoring.js
+++ public/scripts/authoring.js
@@ -1,12 +1,27 @@
 import { getPresentation } from "../modules/presentationHandler.js";
 /****** HTML ELEMENTS ******/
 const presentationTitle = document.getElementById('presentation-title');
+const previews = document.getElementById('previews');
+const imageLeft = document.getElementById('image-left');
+const imageCover = document.getElementById('image-cover');
+const imageRight = document.getElementById('image-right');
 
 /***** GLOBAL VARIABLES *****/
 let presentationId = -1;
 let currentPresentation = null;
 
 /***** EVENT HANDLERS *****/
+imageLeft.addEventListener("click", ()=>{
+  console.log("Image left");
+});
+
+imageCover.addEventListener("click", ()=>{
+  console.log("Image cover");
+});
+
+imageRight.addEventListener("click", ()=>{
+  console.log("Image right");
+});
 
 /***** FUNCTIONS *****/
 window.onload = async () => {
@@ -23,3 +38,4 @@ window.onload = async () => {
   // Set title
   presentationTitle.innerHTML = currentPresentation.presentation_title;
 };
+
